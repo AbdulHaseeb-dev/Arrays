33473c5755f242fdfce27f0c99974a0878b9a4fd
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-03-20 19:47:25 +0500

    Initial commit

---
+++ Marks.java
@@ -0,0 +1,12 @@
+public class Marks {
+    public static void main(String[] args) {
+        int marks [] = new int[3];
+        marks [0] = 97;
+        marks [1] = 95;
+        marks [2] = 99;
+
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
